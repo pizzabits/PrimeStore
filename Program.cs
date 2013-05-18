d1c08d2a554fcddb41314a34ef347e74904c0c3f
Michael Ridner <michael.ridner@gmail.com>
2013-05-18 14:48:25 +0300

    Initiated the store before any users comes in.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -11,6 +12,16 @@ namespace PrimeStore
     {
         public static void Main(string[] args)
         {
+            // initialization must be done before users enters the store,
+            // since the store implementation is (conveniently defined) lazy-initialization.
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            Store.Instance.BuyNumber(0, (numtype) =>
+            {
+                sw.Stop();
+                Console.WriteLine("It took {0} to initialize a store with primes not larger than {1}", sw.Elapsed, Store.MAX_VALUE);
+            });
+
             for (int i = 0; i < 3; i++)
             {
                 Parallel.For(0, Store.MAX_VALUE, (number) =>
